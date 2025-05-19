759702254d904091b55d8fbea2918f0dfc3f5d51
ehsaani <ehsaani.mo@gmail.com>
2025-05-19 18:26:40 +0200

    update

--- creditCard_validator.py
+++ creditCard_validator.py
@@ -1,19 +1,2 @@
-operator = input("Enter the operator (+, -, *, /): ")
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-if operator == '+':
-    result = num1 + num2
-    print(result)
-elif operator == '-':
-    result = num1 - num2
-    print(result)
-elif operator == '*':
-    result = num1 * num2
-    print(result)
-elif operator == '/':
-    result = num1 / num2
-    print(result)
-else:
-    result = "Error! Division by zero."
-    print(result)
+# test
+print("Hi")
